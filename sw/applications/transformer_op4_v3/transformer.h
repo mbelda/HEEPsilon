56e4801a09d744e5b43ba0b72db02fe780cf98b7
mbelda <11842513+mbelda@users.noreply.github.com>
2023-12-13 17:31:01 +0100

    cleaning repo and add transformercpp 32 bit

--- sw/applications/transformer_op4_v3/transformer.h
+++ sw/applications/transformer_op4_v3/transformer.h
@@ -6,9 +6,9 @@
 
 // C multiple of 4x4
 
-#define ROWS_A 160
-#define COLS_A 4
-#define COLS_B 4
+#define ROWS_A 80
+#define COLS_A 80
+#define COLS_B 80
 #define ROWS_B COLS_A
 #define ROWS_C ROWS_A
 #define COLS_C COLS_B
--- sw/applications/transformer_v4/transformer.h
+++ sw/applications/transformer_v4/transformer.h
@@ -7,13 +7,3 @@
 // 121x4x121 
 // 121x121x4
 
-#define ROWS_A 120
-#define COLS_A 400
-#define COLS_B 16
-#define ROWS_B COLS_A
-#define ROWS_C ROWS_A
-#define COLS_C COLS_B
-#define BLOCK_SIZE 3
-
-
-#endif // _STIMULI_H_
