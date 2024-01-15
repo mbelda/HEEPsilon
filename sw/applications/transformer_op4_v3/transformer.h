ff119afe2bc659aa7fac8c77ee321e59ab3931c1
mbelda <11842513+mbelda@users.noreply.github.com>
2024-01-15 10:26:23 +0100

    change dims

--- sw/applications/transformer_op4_v3/transformer.h
+++ sw/applications/transformer_op4_v3/transformer.h
@@ -6,9 +6,9 @@
 
 // C multiple of 4x4
 
-#define ROWS_A 60
-#define COLS_A 60
-#define COLS_B 60
+#define ROWS_A 16
+#define COLS_A 16
+#define COLS_B 16
 #define ROWS_B COLS_A
 #define ROWS_C ROWS_A
 #define COLS_C COLS_B
