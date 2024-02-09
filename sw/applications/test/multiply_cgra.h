f7c0ab98376745c0883e841fb3c2fa4932f256fb
mbelda <11842513+mbelda@users.noreply.github.com>
2024-02-09 11:07:21 +0100

    transformer cgra working

--- sw/applications/test/multiply_cgra.h
+++
@@ -1,6 +0,0 @@
-#include "performance.h"
-
-// Multiply the matrix in the cgra
-void multiply_cgra( void * kperf, int * matrixA, int rowsA, int colsA, int * matrixB, int colsB, int * matrixC);
-void countersInit();
-void initCGRA();
--- sw/applications/test_2/multiply_cgra.h
+++
@@ -1,7 +0,0 @@
-
-#include "performance.h"
-
-// Multiply the matrix in the cgra
-void multiply_cgra( kcom_perf_t * kperf, int * matrixA, int rowsA, int colsA, int * matrixB, int rowsB, int colsB, int * matrixC, int rowsC, int colsC);
-void countersInit();
-void initCGRA();
